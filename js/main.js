ca0388bc15aabe55f7a4c5da5da598284d390dc6
selistym <selistym@protonmail.com>
2019-01-26 03:00:37 -0800

    load external image

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-$('#image-1').rcrop({
-    minSize : [200,200],
-    preserveAspectRatio : true,
+$('#logo-chooser').on("change", e => {
+    var file = e.target.files[0];
 
-    preview : {
-        display: true,
-        size : [100,100],
-    }
+    var reader = new FileReader();
+    reader.onload = function (evt) {
+        $("#img-logo").attr("src", evt.target.result);
+        $('#img-logo').rcrop({
+            minSize: [100, 80],
+            preserveAspectRatio: true,
+        
+            preview: {
+                display: true,
+                size: [100, 80],
+            }
+        });
+    };
+    reader.readAsDataURL(file);
 });
+
